19b42c37992c1e6b37159f03df25fc7b8ac1e6fb
Sowmiyaprs <periyasamysowmiya339@gmail.com>
2024-08-21 14:25:09 +0530

    ok

---
+++ Filecreate.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Filecreate {
+    public static void main(String[] args) {
+        File newFile=new File("sample.txt");
+        try {
+            if (newFile.createNewFile()){
+                System.out.println("File created");
+            }else{
+                System.out.println("File exists");
+            }                           
+        }
+        catch (IOException i){
+            System.out.println("Error");
+        }                                           //the file sample.txt as been created
+                                                    //the next process will be in the next file
+    }
+}
